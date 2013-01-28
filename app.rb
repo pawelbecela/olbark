356502a6d0e503f2010dea96aa343cae85d5fc2a
Pawel Becela <pawbecela@gmail.com>
2013-01-28 18:27:45 +0100

    pony settings

--- app.rb
+++ app.rb
@@ -50,8 +50,8 @@ class App < Sinatra::Base
         :address              => 'smtp.gmail.com',
         :port                 => '587',
         :enable_starttls_auto => true,
-        :user_name            => 'pawbecela@gmail.com',
-        :password             => 'pawbec83',
+        :user_name            => 'arkadiusz.olborski.olbark@gmail.com',
+        :password             => 'olbark123456',
         :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
         :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
       }
